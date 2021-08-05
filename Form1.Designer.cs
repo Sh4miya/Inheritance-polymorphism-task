953fb76afe3b8ddc6f644ebfec884825e1cfe064
Sh4miya <lizzie.corbett04@gmail.com>
2021-08-05 18:43:05 +1200

    added classes

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,71 @@ namespace Inheritance_polymorphism_task
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Result = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Animal Type:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Result
+            // 
+            this.Result.Location = new System.Drawing.Point(270, 12);
+            this.Result.Multiline = true;
+            this.Result.Name = "Result";
+            this.Result.Size = new System.Drawing.Size(409, 108);
+            this.Result.TabIndex = 3;
+            this.Result.TextChanged += new System.EventHandler(this.Result_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 139);
+            this.Controls.Add(this.Result);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Animal Data";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Result;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -22,28 +22,74 @@ namespace Inheritance_polymorphism_task
         {
 
         }
-    }
 
-    class Animal
-    {
-        public string type { get; set; }
-        public int ID { get; set; }
-        public double price { get; set; }
-    }
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-    class Cow : Animal
+        }
 
-    {
-        public string cow;
-        public string jerseyCow;
+        private void Result_TextChanged(object sender, EventArgs e)
+        {
 
-    }
+        }
 
-    class Goat : Animal
-    {
-        public string goat;
-    }
 
-                        
+        public class Animal
+        {
+            public string Type { get; set; }
+            public int Id { get; set; }
+            public double Price { get; set; }
+
+            public Animal(string type, int id, double price)
+            {
+                Type = type;
+                Id = id;
+                Price = price;
+            }
+        }
+
+        public class Cow : Animal
+        {
+            public Cow(Animal animal, string cow) : base(animal.Type, animal.Id, animal.Price)
+
+            {
+                this.cow = cow;
+            }
+
+            public string cow { get; set; }
+            //public string jCow { get; set; }
+
+        }
+
+        public class Goat : Animal
+        {
+            public Goat(Animal animal, string goat) : base(animal.Type, animal.Id, animal.Price)
+            {
+                this.goat = goat;
+            }
+            public string goat { get; set; }
+        }
+
+        static class Stats
+        {
+            static void Main(String[] args)
+            {
+                string[] data = System.IO.File.ReadAllLines(@"C:\Users\the_1\source\repos\Inheritance-polymorphism-task\task-data.txt");
+
+                try
+                {
+                    Animal animal = new Animal(data);
+                }
+
+                foreach (string line in data)
+                {
+                    string result = "\t" + line;                   
+                }
+
+            }
+
+        }
+
+    }                  
 }
 
