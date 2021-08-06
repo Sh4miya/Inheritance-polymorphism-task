a7109ea7eaa2021c657fe07b5e2e905a62c276d6
Sh4miya <lizzie.corbett04@gmail.com>
2021-08-06 15:28:47 +1200

    Edit classes, added more code

---
+++ Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_polymorphism_task
+{
+    abstract class Animal
+    {
+        public int id;
+
+        public Animal(int id)
+        {
+            this.id = id;
+        }
+
+        public abstract double getProf();
+
+    }
+
+}
---
+++ JerseyCow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_polymorphism_task
+{
+    class JerseyCow : Cow
+    {
+        public JerseyCow(int id, double milkAmt) : base(id, milkAmt)
+        {
+
+        }
+        override public double getProf()
+        {
+            return (0);
+        }
+    }
+}
